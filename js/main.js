02bf9ed130fc8ce50de82602fc3cca9a0eba8ff5
JakubKarp <jakub.karp@innespacery.pl>
2017-09-15 10:54:16 +0200

    last update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-
 window.addEventListener('load', function() {
+
   
   // video container
   video = document.getElementById('video');
@@ -12,15 +12,25 @@ window.addEventListener('load', function() {
   // button container
   playButton = document.getElementById('play-button');
   timeField = document.getElementById('time-field');
+  soundButton = document.getElementById('sound-button');
+  sbarContainer = document.getElementById('sbar-container');
+  sbar = document.getElementById('sbar');
+  fullscreenButton = document.getElementById("full-screen");
   
+  
+ 
   video.load();
   video.addEventListener('canplay', function() {
     playButton.addEventListener('click', playOrPause, false);
     }, false);
     pbarContainer.addEventListener('click', skip, false); 
-    updatePlayer();  
-  
-  
+    video.addEventListener('durationchange', function() {
+        updatePlayer();
+    });
+    soundButton.addEventListener('click', muteOrUnmute, false);
+    sbarContainer.addEventListener('click', changeVolume, false);  
+    fullscreenButton.addEventListener('click', fullscreen, false);
+   
 }, false);
 
 // function play or pause and change icons and set Interval
@@ -60,18 +70,62 @@ function skip(ev) {
   
 };
 
-// function for counting time of video
+
+// function for showing counting time of video
 function getFormattedTime() {
+
   var seconds = Math.round(video.currentTime); //give current time rounded to 1
   var minutes = Math.floor(seconds/60); // give minutes from seconds floored 
   if (minutes > 0) seconds -= minutes*60; // give only 60 seconds for minute
   if (seconds.toString().length === 1) seconds = '0' + seconds; // give zero before 
   
   // for total duration - almost the same code
-  var totalSeconds = Math.round(video.duration); //duration gives NaN if video is not set https://github.com/videojs/video.js/issues/2017 
+    var totalSeconds = Math.round(video.duration); //duration gives NaN if video is not set https://github.com/videojs/video.js/issues/2017 
   var totalMinutes = Math.floor(totalSeconds/60); 
   if (totalMinutes > 0) totalSeconds -= totalMinutes*60; 
   if (totalSeconds.toString().length === 1) totalSeconds = '0' + totalSeconds; 
   
     return minutes + ':' + seconds + ' / ' + totalMinutes + ':' + totalSeconds;
+};
+
+
+// function for muting sound
+function muteOrUnmute() {
+  if (!video.muted) {
+    video.muted = true;
+    soundButton.src = 'images/mute.png';
+    sbar.style.display = 'none';
+  } else {
+    video.muted = false;
+    soundButton.src = 'images/sound.png';
+    sbar.style.display = 'block';
+  }
+};
+
+
+// function for change volume by sound bar
+function changeVolume(ev) {
+  var mouseX = ev.pageX - sbar.offsetLeft;
+  var width = window.getComputedStyle(sbarContainer).getPropertyValue('width') //takes value of width of bar
+  width = parseFloat(width.substr(0, width.length - 2)); //changes from string to number
+  
+  video.volume = (mouseX/width);
+  sbar.style.width = (mouseX/width) * 100 + '%';
+  video.muted = false; 
+  soundButton.src = 'images/sound.png';
+  sbar.style.display = 'block';
+};
+
+//function for fullscreen
+function fullscreen() {
+  if (video.webkitRequestFullScreen) {
+    video.webkitRequestFullScreen();
+  } else if (video.mozRequestFullScreen) {
+    video.mozRequestFullScreen();
+  } else if (video.msRequestFullscreen) {
+    video.msRequestFullscreen();
+  } else if (video.requestFullscreen) {
+    video.requestFullscreen();
+  }
+  
 };
